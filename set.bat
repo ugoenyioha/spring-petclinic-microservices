a9f616086491104f2bec26573f031ba7a0fb770e
Ugo Enyioha <ugo.enyioha@outlook.com>
2017-12-09 21:41:22 -0800

    being asked to commit changes

---
+++ set.bat
@@ -0,0 +1,6 @@
+SET DOCKER_TLS_VERIFY=1
+SET DOCKER_HOST=tcp://192.168.1.39:2376
+SET DOCKER_CERT_PATH=C:\Users\ugoen\.minikube\certs
+SET DOCKER_API_VERSION=1.23
+REM Run this command to configure your shell:
+REM @FOR /f "tokens=*" %i IN ('minikube docker-env') DO @%i
